78272a5794beb13d8cbeb5627d01f39f4a7e0451
SviatoslavIshchenko <isv.emm@gmail.com>
2026-08-12 19:56:41 +0300

    Delete line 8 (#1)
    * Add 5+5
    
    * Create branch feacher_login. Add Hello world
    
    * Add 5+5+5
    
    * Add git remote
    
    * Remove 5+5+5
    
    * New line in main
    
    * Merge main <- new
    
    * Change row 5
    
    * Change row 10
    
    * change line 7
    
    * Add lines in new py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # сірий - поточний файл без змін, гіт бачить його його
 # git init - ініціалізація гіта, щоб гіт слідкував за проектом
 # git status - інформація поточного стану проекту
+# git remote -v - чи підв'язані до Гітхабу
 # git branch -M main - пеерейменування головної гілки
 # git add main.py - додавання файлу до гіта
 # git commit -m "Initial commit" - коміт файлу (зміна) і коментар
@@ -16,8 +17,19 @@
 # origin - посилання на репозиторій https://github.com/SviatoslavIshchenko/git-practice.git
 # git remote add origin https://github.com/SviatoslavIshchenko/git-practice.git
 # git-learning> git remote -v - підключення до сервера і отримання інформації
-# git push -u origin main
+# git push -u origin main - відправлення оновлених файлів на Гітхаб при першому відправленні
+# git push -u origin new - відправлення нової гілки на Гітхаб
 # git pull - відправлення коду на локальний компютер
+# git push - відправлення оновлених файлів на Гітхаб
+# git diff - показати різницю між файлами в більшості на сервері
+# git branch feacher-login - Створення нової гілки
+# git switch feacher-login - переключення на гілку
+# git-practice> git push origin --delete feacher-login - видалення гілки
 
 print("Hello Git!!!")
 
+print("Hello Git!!!")
+
+print("Hello Git!!!  111")
+
+print("Hello Git!!!  111")
